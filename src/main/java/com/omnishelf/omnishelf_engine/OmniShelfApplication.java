116ff6898e5c7b889d52bc10175d92b0b47a3685
navneetyadavv <navneetyadav49710@gmail.com>
2026-05-19 21:58:13 +0530

    first commit

---
+++ src/main/java/com/omnishelf/omnishelf_engine/OmniShelfApplication.java
@@ -0,0 +1,13 @@
+package com.omnishelf.omnishelf_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniShelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmniShelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omnishelf/omnishelf_engine/OmniShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omnishelf.omnishelf_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmniShelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
